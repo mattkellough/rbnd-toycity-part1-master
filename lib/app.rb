47bcff4e40913470e2f6640c5acfb56172c6868d
Matt Kellough <mrk10a@gmail.com>
2016-03-24 14:50:37 -0400

    Converted full price to float

--- lib/app.rb
+++ lib/app.rb
@@ -16,34 +16,39 @@ puts "|_|                                       "
 
 
 # For each product in the data set:
-  products_hash["items"].each do |toy|
+products_hash["items"].each do |toy|
   # Print the name of the toy
-    puts "#{toy["title"]}"
+  puts "#{toy["title"]}"
   # Print the retail price of the toy
-    puts "Retail Price: $#{toy["full-price"]}"
+  retail = toy["full-price"]
+  puts "Retail Price: $#{retail}"
   # Calculate and print the total number of purchases
-    puts "Purchases: #{(toy["purchases"]).length}"
+  amt_purchases = toy["purchases"].length
+  puts "Purchases: #{amt_purchases}"
   # Calculate and print the total amount of sales
   revenue = 0.0
-    toy["purchases"].each do |purchases|
-      revenue = revenue + purchases["price"]
-    end
-      puts "Sales: $#{revenue}"
+  toy["purchases"].each do |purchases|
+    revenue = revenue + purchases["price"]
+  end
+  puts "Sales: $#{revenue}"
   # Calculate and print the average price the toy sold for
+  avg_price = revenue / amt_purchases
+  puts "Average Price: $#{avg_price}"
   # Calculate and print the average discount (% or $) based off the average sales price
 
+end
 
-	puts " _                         _     "
-	puts "| |                       | |    "
-	puts "| |__  _ __ __ _ _ __   __| |___ "
-	puts "| '_ \\| '__/ _` | '_ \\ / _` / __|"
-	puts "| |_) | | | (_| | | | | (_| \\__ \\"
-	puts "|_.__/|_|  \\__,_|_| |_|\\__,_|___/"
-	puts
+
+puts " _                         _     "
+puts "| |                       | |    "
+puts "| |__  _ __ __ _ _ __   __| |___ "
+puts "| '_ \\| '__/ _` | '_ \\ / _` / __|"
+puts "| |_) | | | (_| | | | | (_| \\__ \\"
+puts "|_.__/|_|  \\__,_|_| |_|\\__,_|___/"
+puts
 
 # For each brand in the data set:
-  # Print the name of the brand
-  # Count and print the number of the brand's toys we stock
-  # Calculate and print the average price of the brand's toys
-  # Calculate and print the total revenue of all the brand's toy sales combined
-end
+# Print the name of the brand
+# Count and print the number of the brand's toys we stock
+# Calculate and print the average price of the brand's toys
+# Calculate and print the total revenue of all the brand's toy sales combined
